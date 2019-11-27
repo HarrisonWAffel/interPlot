f68520e61feebd9c5a90a2b17d199ba1b0ba95c8
harrisonWAffel <affelh@wit.edu>
2019-11-26 23:51:18 -0500

    Go routine implemented to speed up http connections

--- main.go
+++ main.go
@@ -7,43 +7,47 @@ import (
 	"github.com/flopp/go-staticmaps"
 	"github.com/fogleman/gg"
 	"github.com/golang/geo/s2"
+
 	"image/color"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 type Ip struct {
-	IP     string `json:"ip"`
-	Geoip2 struct {
-		City struct {
-			Name string `json:"name"`
-			ID   int    `json:"id"`
-		} `json:"city"`
-		Country struct {
-			Name string `json:"name"`
-			Code string `json:"code"`
-			ID   int    `json:"id"`
-		} `json:"country"`
-		Continent struct {
-			Name string `json:"name"`
-			Code string `json:"code"`
-			ID   int    `json:"id"`
-		} `json:"continent"`
-		Postal struct {
-			Code string `json:"code"`
-		} `json:"postal"`
-		Latlong struct {
-			AccuracyRadius int     `json:"accuracy_radius"`
-			Latitude       float64 `json:"latitude"`
-			Longitude      float64 `json:"longitude"`
-			MetroCode      int     `json:"metro_code"`
-			TimeZone       string  `json:"time_zone"`
-		} `json:"latlong"`
-		Metadata struct {
-		} `json:"metadata"`
-	} `json:"geoip2"`
+	IP            string  `json:"ip"`
+	Type          string  `json:"type"`
+	ContinentCode string  `json:"continent_code"`
+	ContinentName string  `json:"continent_name"`
+	CountryCode   string  `json:"country_code"`
+	CountryName   string  `json:"country_name"`
+	RegionCode    string  `json:"region_code"`
+	RegionName    string  `json:"region_name"`
+	City          string  `json:"city"`
+	Zip           string  `json:"zip"`
+	Latitude      float64 `json:"latitude"`
+	Longitude     float64 `json:"longitude"`
+	Location      struct {
+		GeonameID int    `json:"geoname_id"`
+		Capital   string `json:"capital"`
+		Languages []struct {
+			Code   string `json:"code"`
+			Name   string `json:"name"`
+			Native string `json:"native"`
+		} `json:"languages"`
+		CountryFlag             string `json:"country_flag"`
+		CountryFlagEmoji        string `json:"country_flag_emoji"`
+		CountryFlagEmojiUnicode string `json:"country_flag_emoji_unicode"`
+		CallingCode             string `json:"calling_code"`
+		IsEu                    bool   `json:"is_eu"`
+	} `json:"location"`
 }
+
+const ApiKey = "959279e06d5ca3430cf26fdbca17ea7c"
 
 func main() {
 	//run and wait for zmap
@@ -53,8 +57,8 @@ func main() {
 	//use static plot to plot the long lat
 	//print the resulting picture
 	//at somepoint we should be able to specify locations to scan
-
-	scanInternet()
+	getIpLocations()
+	//scanInternet()
 
 }
 
@@ -62,7 +66,7 @@ func main() {
 func scanInternet() {
 	fmt.Println("Scanning")
 	cm := "zmap"
-	args := []string{"-B", "200M", "-p", "21", "-n", "7000000", "-o", "results.csv"}
+	args := []string{"-B", "2M", "-p", "21", "-n", "700", "-o", "test.csv"}
 
 	cmd := exec.Command(cm, args...)
 	//We need to create a reader for the stdout of this script
@@ -75,12 +79,17 @@ func scanInternet() {
 	//A scanner is created to read the stdout of the above command
 	scanner := bufio.NewScanner(cmdReader)
 	defer cmdReader.Close()
+
 	//A new go thread is created to handle the output
 	go func() {
-		fmt.Print("ok")
-		for scanner.Scan() {
+		for {
 
-			fmt.Println(scanner.Text())
+			fmt.Print("ok")
+			for scanner.Scan() {
+				//todo; make this output zmap
+				fmt.Println(scanner.Text())
+
+			}
 		}
 	}()
 
@@ -91,49 +100,74 @@ func scanInternet() {
 		os.Exit(1)
 	}
 	fmt.Print(scanner.Text())
-	getIpLocations()
+
 }
 
-//zan is a command line tool that is used to find the lat long position of each ip.
+//getIpLocations is a function that utilizes the ipstack api for ip geolocation. It utilizes a simple curl command to get a json response body containing the desired information.
 func getIpLocations() {
 
-	plotPoints(createIps())
-}
-
-//createIps is a function that reads a zannotated json file and creates an array of Ip structs
-func createIps() []Ip {
-
-	jsonout, err := exec.Command("cat", "out.json").Output() //Just for testing, replace with your subProcess
+	lines, err := ioutil.ReadFile("results.csv")
 	if err != nil {
-		panic(3)
+		fmt.Print("couldn't read results file")
+		panic(0)
 	}
 
-	lines := strings.Split(string(jsonout), "\n")
+	ipStrings := strings.Split(string(lines), "\n")
 
-	ips := make([]Ip, len(lines))
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
 
-	for i, e := range lines {
-		errs := json.Unmarshal([]byte(e), &ips[i])
-		if errs != nil {
-			fmt.Print(ips)
-			fmt.Print(errs)
+	ips := make([]Ip, len(ipStrings))
+	fmt.Println("Getting Location Of IPS.")
+	total := len(ipStrings)
 
-		}
+	for i, e := range ipStrings {
+		fmt.Print("Located ")
+		fmt.Print(i)
+		fmt.Print(" out of ")
+		fmt.Print(total)
+		fmt.Println(" Ip addresses")
+
+		go func() {
+			response, err := netClient.Get("http://api.ipstack.com/" + e + "?access_key=" + ApiKey + "&output=json")
+			if err != nil {
+				fmt.Println("bad APi connection reported")
+			}
+			defer response.Body.Close()
+			if response.StatusCode == http.StatusOK {
+				bodyBytes, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				ip := Ip{}
+				e := json.Unmarshal(bodyBytes, &ip)
+				if e != nil {
+					fmt.Println(e)
+
+				}
+
+				ips = append(ips, ip)
+			}
+			return
+		}()
 
 	}
 
-	fmt.Println(ips)
-	return ips
+	//https://ipstack.com/quickstart
+
+	plotPoints(ips)
 }
 
-//plotPoints uses static maps to create an output image
+//plotPoints uses static maps to create an output image, it takes  an array of Ip structs as its only parameter
 func plotPoints(ips []Ip) {
 
 	ctx := sm.NewContext()
 	ctx.SetSize(4000, 3000)
 
 	for i, _ := range ips {
-		ctx.AddMarker(sm.NewMarker(s2.LatLngFromDegrees(ips[i].Geoip2.Latlong.Latitude, ips[i].Geoip2.Latlong.Longitude), color.RGBA{0xff, 0, 0, 0xff}, 16.0))
+		ctx.AddMarker(sm.NewMarker(s2.LatLngFromDegrees(ips[i].Latitude, ips[i].Longitude), color.RGBA{0xff, 0, 0, 0xff}, 16.0))
 	}
 
 	img, err := ctx.Render()
